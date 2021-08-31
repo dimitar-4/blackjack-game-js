75c6160872e44f513f2dc51a79f3f6f4c95d3e79
dimitar-4 <dcholakov4@gmail.com>
2021-08-31 11:39:54 +0000

    Remove player variable in the js file. Add container div and remove player paragraph from the html file

--- scripts.js
+++ scripts.js
@@ -6,9 +6,6 @@ let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
-let playerEl = document.getElementById('player-el')
-
-playerEl.textContent = player.name + ': $' + player.chips
 
 function getRandomCard() {
     let randomCard = Math.floor(Math.random() * 13) + 1
